86639b4af2064f72759d1cdf83d6895aa1e15432
semaDilthey <gaidsemen@yandex.ru>
2024-08-08 21:42:24 +0300

    first commit

---
+++ WB_FinalProject/WB_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB_FinalProjectApp.swift
+//  WB_FinalProject
+//
+//  Created by Семен Гайдамакин on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
